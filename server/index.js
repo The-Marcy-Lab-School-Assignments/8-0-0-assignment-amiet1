733f30fda6b991bbdcade4e69149fe2925eef83f
benspector-mls <ben@marcylabschool.org>
2024-04-09 15:34:57 -0400

    code graded: 12/13 tests passing

--- server/index.js
+++ server/index.js
@@ -1,26 +1,34 @@
-const express = require('express');  
+const express = require('express');
 const app = express();
 
 
-const serveAbout = (req,res,next) => res.send('<h1>Hello Tahj</h1>')
+const serveAbout = (req, res, next) => res.send('<h1>Hello Tahj</h1>')
 
-const servePersonal = (req,res,next) => res.send('<h1> Where are you from?</h1>')
+const servePersonal = (req, res, next) => res.send('<h1> Where are you from?</h1>')
 
 //* Two controllers send a data response
 
-    const serveData = (req, res, next) => {
-    const data = [{ name: 'Tahj' }, { age: '21' }, { favColor: 'blue' }];
-    res.send(data);
-    }
+/* FEEDBACK: The data you're sending back could have 
+  a better structure. You're returning an array of separate
+  objects, but those objects all have a single data value
+  related to you. Instead, just send back a single object:
+  
+  const data = { name: 'Tahj', age: 21, favColor: 'blue'}
+  */
+const serveData = (req, res, next) => {
+  const data = [{ name: 'Tahj' }, { age: '21' }, { favColor: 'blue' }];
+  res.send(data);
+}
 
-    const serveHello = (req, res, next) => {
-        const name = req.query.name || "stranger";
-        res.send(`hello ${name}`);
-    }
+/* FEEDBACK: Nice! */
+const serveHello = (req, res, next) => {
+  const name = req.query.name || "stranger";
+  res.send(`hello ${name}`);
+}
 
 
 app.get('/about', serveAbout);
-app.get('/personal',servePersonal);
+app.get('/personal', servePersonal);
 app.get('/api/data', serveData);
 app.get('/api/hello', serveHello);
 
